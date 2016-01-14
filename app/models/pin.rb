475e82c657cb7b59e70b06cd7635bb54bce79368
justinav <justinavil@gmail.com>
2016-01-14 13:07:55 -0500

    adding category model and fixing small bugs

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,4 +1,7 @@
 class Pin < ActiveRecord::Base
-  validates_presence_of :title, :url, :slug, :text, :resource_type
+  validates_presence_of :title, :url, :slug, :text
   validates_uniqueness_of :slug
+
+  belongs_to :category
+
 end
