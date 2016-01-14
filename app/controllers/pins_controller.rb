65929aa9f2e96ad4dfa5cd19a17b8388c54072e7
justinav <justinavil@gmail.com>
2016-01-13 21:40:56 -0500

    adding pretty url route and tests

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -2,10 +2,16 @@ class PinsController < ApplicationController
   
   def index
     @pins = Pin.all
+    @pin = Pin.find_by_slug(params[:slug])
   end
-  
+
+  def show_by_name
+  	@pin = Pin.find_by_slug(params[:slug])
+  	render :show
+  end
+
   def show
     @pin = Pin.find(params[:id])
   end
-  
+ 
 end
