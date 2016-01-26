1badf7d8df62906c2f9b8d8577c2c4faa581da29
justinav <justinavil@gmail.com>
2016-01-25 21:27:52 -0500

    adding AWS credentials

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,4 +41,13 @@ Rails.application.configure do
 
   # Command line for Paperclip
   Paperclip.options[:command_path] = "/usr/local/bin/convert"
+
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => "skillcrush-rails",
+      :access_key_id => "AKIAI4Y7CIUPPOFAYN2Q",
+      :secret_access_key => "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"
+    }
+  }
 end
