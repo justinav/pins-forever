3cdbf5e5a1c5aa7096093762c0d173e2e80f396d
justinav <justinavil@gmail.com>
2016-03-03 16:52:40 -0500

    updating routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,16 +4,12 @@ Rails.application.routes.draw do
   get 'signup' => "users#new", as: :signup
   get 'login' => "users#login", as: :login
   post 'login' => "users#authenticate"
+  delete 'logout/:id' => "users#logout", as: :logout
 
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
   root 'pins#index'
-
   get "pins/name-:slug" => "pins#show_by_name", as: 'pin_by_name'
+  get '/library' => 'pins#index'
 
   resources :pins
 
-  get '/library' => 'pins#index'
 end
