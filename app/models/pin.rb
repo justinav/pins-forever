5749a1e9dbb4b05960269439a2a158eabb6e4f40
justinav <justinavil@gmail.com>
2016-03-03 16:51:04 -0500

    adding user associations

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,8 +2,9 @@ class Pin < ActiveRecord::Base
   validates_presence_of :title, :url, :slug, :text
   validates_uniqueness_of :slug
 
+  belongs_to :user
   belongs_to :category
 
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "60x60>" }, default_url: "http://placebear.com/300/300" 
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "60x60>" }, default_url: "http://placebear.com/300/300"
   validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
 end
